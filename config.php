a5951425e21c5c5fd992be2ed9faf9ac702252fe
Redon <Redon.Bytyqi@student.cacttus.education>
2024-11-23 16:59:34 +0100

    configuration with db

--- config.php
+++ config.php
@@ -1,4 +1,24 @@
 <?php
+     
+     session_start();
+      
+    $host = "localhost"; 
+    $user = "root";
+    $pass = "";
+    $dbname = "db";
+
+    try{
+        $conn = new PDO("mysql:host=$host; dbname=$dbname", $user, $pass);
+
+    }
+
+    catch(Exception $e){
+        echo "Error: " . $e->getMessage();
+    }
+
+
+
+
 
   
 
