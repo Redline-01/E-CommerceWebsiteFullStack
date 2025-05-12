a0901ae3483af49e9217a5f6b4a01746c9507e3b
Redon <Redon.Bytyqi@student.cacttus.education>
2025-05-12 16:11:48 +0200

    .

--- signup.php
+++ signup.php
@@ -28,7 +28,7 @@
      /*form*/
      .form-container {
          min-height: 80vh; 
-         width: 95vw; 
+         width: 100%; 
          display: flex;
          align-items: center; 
          justify-content: center; 
