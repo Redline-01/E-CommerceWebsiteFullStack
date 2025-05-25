f21220f09a2c745c24ef5a389bd3a3168d209b10
Redon <Redon.Bytyqi@student.cacttus.education>
2025-05-25 20:20:40 +0200

    added the logo on dashboard

--- messagesDashboard.php
+++ messagesDashboard.php
@@ -34,11 +34,13 @@
  <body>
  
  
- <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"><?php echo "Welcome to Dashboard ".$_SESSION['username']; ?></a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+ <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow" style="position:relative; min-height:65px;">
+   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"><?php echo "Welcome to Dashboard ".$_SESSION['username']; ?></a>
+  <img src="images/hyperx.png" alt="HyperX Logo" style="position:absolute; left:50%; top:50%; transform:translate(-50%,-50%); height:60px; z-index:10;">
+  
   
   <div class="navbar-nav">
     <div class="nav-item text-nowrap">
